ebfc1ec95baff8ecbad675f366fe21a3c35796c4
Huzaifa Khan <113866051+Huzaifakhan3@users.noreply.github.com>
2026-02-03 04:58:30 +0000

    -change back to gitpage

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 export default defineConfig({
     plugins: [react()],
-    base: "/",
+    base: "/portfolio/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
